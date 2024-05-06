4699131289e40e6e5b7848e19f94e2ccdd6082f0
hanaMohammedAbdullah <hana.muhammed.abdulla@gmail.com>
2024-05-06 22:36:48 +0300

    darkmode cv

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,16 @@
 import video from "../data/video.js";
-
 function App() {
   console.log("Here's your data:", video);
 
   return (
-    <div className="App">
+    <div className='App'>
       <iframe
-        width="919"
-        height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        frameBorder="0"
+        width='919'
+        height='525'
+        src='https://www.youtube.com/embed/dQw4w9WgXcQ'
+        frameBorder='0'
         allowFullScreen
-        title="Thinking in React"
+        title='Thinking in React'
       />
     </div>
   );
